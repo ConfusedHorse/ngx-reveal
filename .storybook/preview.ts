5493f50c4ffe8a7c67d932042b602119abad3e0f
Confused Horse <21200630+ConfusedHorse@users.noreply.github.com>
2023-10-29 09:58:49 +0100

    set default background

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,11 +3,8 @@ import type { Preview } from "@storybook/angular";
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
+    backgrounds: {
+      default: 'dark',
     },
   },
 };
